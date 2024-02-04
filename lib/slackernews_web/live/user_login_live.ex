98e0682eb9c42f8d60fc99471aed4f60a1633227
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2024-02-05 00:59:07 +0100

    Fix invalid input type

--- lib/slackernews_web/live/user_login_live.ex
+++ lib/slackernews_web/live/user_login_live.ex
@@ -16,7 +16,7 @@ defmodule SlackernewsWeb.UserLoginLive do
       </.header>
 
       <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
-        <.input field={@form[:username]} type="Username" label="Username" required />
+        <.input field={@form[:username]} type="text" label="Username" required />
         <.input field={@form[:password]} type="password" label="Password" required />
 
         <:actions>
--- lib/slackernews_web/live/user_registration_live.ex
+++ lib/slackernews_web/live/user_registration_live.ex
@@ -31,7 +31,7 @@ defmodule SlackernewsWeb.UserRegistrationLive do
           Oops, something went wrong! Please check the errors below.
         </.error>
 
-        <.input field={@form[:username]} type="username" label="Username" required />
+        <.input field={@form[:username]} type="text" label="Username" required />
         <.input field={@form[:email]} type="email" label="Email" />
         <.input field={@form[:password]} type="password" label="Password" required />
 
