4f59ebf54716d9290b261a1e84b1ba6606d7e10a
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2024-02-05 08:16:30 +0100

    Add PostLive.ShowComponent

---
+++ lib/slackernews_web/live/post_live/show_component.ex
@@ -0,0 +1,32 @@
+defmodule SlackernewsWeb.PostLive.ShowComponent do
+  use SlackernewsWeb, :live_component
+
+  alias Slackernews.Posts
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+      <div class="my-4 flex items-center">
+        <.live_component
+          module={SlackernewsWeb.PostLive.VoteComponent}
+          user_id={@current_user && @current_user.id}
+          id={@post.id}
+        />
+        <div>
+          <a href={@post.url} class="font-medium"><%= @post.title %></a>
+          <div class="text-zinc-700 text-sm">
+            <span><%= @post.author.username %></span>
+            | <.timestamp obj={@post}/>
+            | <.link navigate={"/posts/#{@post.id}"}><%= @post.comment_count %> comments</.link>
+            <%= if Posts.can_edit(@current_user, @post) do %>
+            | <.link :if={Posts.can_edit(@current_user, @post)} patch={~p"/posts/#{@post}/show/edit"} phx-click={JS.push_focus()}>
+                edit
+              </.link>
+            <% end %>
+          </div>
+        </div>
+      </div>
+    """
+  end
+
+end
